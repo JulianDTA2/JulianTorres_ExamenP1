9e62bc0321f437e55fe548a0f7327cf1328139cf
JulianTorresDev <julian.torres.dev@gmail.com>
2024-10-28 09:33:37 -0500

    Add project files.

---
+++ Models/Tabla.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JulianTorres_ExamenP1.Models
+{
+    public class Tabla
+    {
+        public int TablaId { get; set; }
+        [Required]
+        public string TablaName { get; set;}
+        public float TablaValue { get; set; }
+        public bool IsTabla { get; set; }
+        public DateOnly DateOnly { get; set; }
+    }
+}
